a7263e29fb115727335ee5d1cfdcb091da2e45f2
Adam Hawke <ah14275@my.bristol.ac.uk>
2022-12-31 14:37:40 +0000

    Refined EditArtistFragment.kt

--- app/src/main/java/com/codersguidebook/supernova/ui/artist/EditArtistFragment.kt
+++ app/src/main/java/com/codersguidebook/supernova/ui/artist/EditArtistFragment.kt
@@ -1,26 +1,24 @@
 package com.codersguidebook.supernova.ui.artist
 
 import android.os.Bundle
-import android.text.Editable
 import android.text.SpannableStringBuilder
 import android.view.*
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import com.codersguidebook.supernova.MainActivity
-import com.codersguidebook.supernova.MusicDatabase
+import androidx.viewbinding.ViewBinding
 import com.codersguidebook.supernova.R
 import com.codersguidebook.supernova.databinding.FragmentEditArtistBinding
 import com.codersguidebook.supernova.entities.Song
+import com.codersguidebook.supernova.fragment.BaseFragment
 
-class EditArtistFragment : Fragment() {
+class EditArtistFragment : BaseFragment() {
+
+    override var _binding: ViewBinding? = null
+        get() = field as FragmentEditArtistBinding?
+    override val binding: FragmentEditArtistBinding
+        get() = _binding!! as FragmentEditArtistBinding
 
     private var artistName: String? = null
-    private var _binding: FragmentEditArtistBinding? = null
-    private val binding get() = _binding!!
-    private var musicDatabase: MusicDatabase? = null
-    private var artistsSongs = emptyList<Song>()
-    private lateinit var callingActivity: MainActivity
+    private var artistSongs = emptyList<Song>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,20 +31,24 @@ class EditArtistFragment : Fragment() {
         }
 
         _binding = FragmentEditArtistBinding.inflate(inflater, container, false)
+
+        binding.editArtistName.text = SpannableStringBuilder(artistName)
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         setHasOptionsMenu(true)
-        
-        callingActivity = activity as MainActivity
-        
-        musicDatabase = MusicDatabase.getDatabase(requireContext(), lifecycleScope)
-        musicDatabase!!.musicDao().findArtistsSongs(artistName!!)
-            .observe(viewLifecycleOwner) {
-                artistsSongs = it
+
+        artistName?.let { name ->
+            musicLibraryViewModel.setActiveArtistName(name)
+
+            musicLibraryViewModel.activeArtistSongs.observe(viewLifecycleOwner) { songs ->
+                artistSongs = songs
             }
-
-        val editable: Editable = SpannableStringBuilder(artistName)
-        binding.editArtistName.text = editable
-
-        return binding.root
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -68,8 +70,8 @@ class EditArtistFragment : Fragment() {
                     newName == artistName -> Toast.makeText(activity, getString(R.string.artist_name_not_changed),
                         Toast.LENGTH_SHORT).show()
                     else -> {
-                        for (song in artistsSongs) song.artist = newName
-                        callingActivity.updateSongs(artistsSongs)
+                        for (song in artistSongs) song.artist = newName
+                        mainActivity.updateSongs(artistSongs)
                         Toast.makeText(activity, getString(R.string.artist_updated),
                             Toast.LENGTH_SHORT).show()
                     }
@@ -80,10 +82,5 @@ class EditArtistFragment : Fragment() {
 
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
