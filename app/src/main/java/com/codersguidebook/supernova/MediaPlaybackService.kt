da22445e4c0ef95a019ce28f715614c08a51667f
Adam <adamrh48@outlook.com>
2025-09-07 15:08:25 +0100

    Refined media notification

--- app/src/main/java/com/codersguidebook/supernova/MediaPlaybackService.kt
+++ app/src/main/java/com/codersguidebook/supernova/MediaPlaybackService.kt
@@ -1,22 +1,30 @@
 package com.codersguidebook.supernova
 
+import android.app.PendingIntent
 import android.content.*
+import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK
 import android.media.AudioAttributes
 import android.media.AudioFocusRequest
 import android.media.AudioManager
 import android.media.AudioManager.*
 import android.media.MediaPlayer.*
+import android.net.Uri
 import android.os.*
 import android.provider.MediaStore
-import android.support.v4.media.session.PlaybackStateCompat.*
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import androidx.media3.session.legacy.MediaMetadataCompat
+import com.codersguidebook.supernova.params.MediaServiceConstants.Companion.ALBUM_ID
+import com.codersguidebook.supernova.params.MediaServiceConstants.Companion.NOTIFICATION_CHANNEL_ID
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
 
 /**
  * DOCUMENTATION
@@ -29,6 +37,7 @@ class MediaPlaybackService : MediaSessionService(), MediaSession.Callback {
     private lateinit var audioFocusRequest: AudioFocusRequest
     private lateinit var player: Player
 
+    private lateinit var artworkDirectory: File
     private var mediaSession: MediaSession? = null
 
     private val afChangeListener = OnAudioFocusChangeListener { focusChange ->
@@ -49,11 +58,19 @@ class MediaPlaybackService : MediaSessionService(), MediaSession.Callback {
     override fun onCreate() {
         super.onCreate()
 
+        artworkDirectory = ContextWrapper(applicationContext).getDir("albumArt", Context.MODE_PRIVATE)
+
         player = ExoPlayer.Builder(this).build().also {
             it.addListener(playerListener)
             it.setHandleAudioBecomingNoisy(true)
         }
-        mediaSession = MediaSession.Builder(this, player).setCallback(this).build()
+
+        val intent = packageManager
+            .getLaunchIntentForPackage(packageName)
+            ?.setPackage(null)
+            ?.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED)
+        val activityIntent = PendingIntent.getActivity(applicationContext, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        mediaSession = MediaSession.Builder(this, player).setCallback(this).setSessionActivity(activityIntent).build()
     }
 
     private fun requestAudioFocus() {
@@ -75,12 +92,43 @@ class MediaPlaybackService : MediaSessionService(), MediaSession.Callback {
         mediaItems: MutableList<MediaItem>
     ): ListenableFuture<MutableList<MediaItem>> {
         val updatedMediaItems = mediaItems.map {
+            val albumId = it.mediaMetadata.extras?.getString(ALBUM_ID)
+            val metadata = if (albumId != null) {
+                val imageFile = File(artworkDirectory, "$albumId.jpg")
+                val artworkUri = Uri.fromFile(imageFile)
+                it.mediaMetadata.buildUpon().setArtworkUri(artworkUri).build()
+            } else null
+
             val uri = ContentUris.withAppendedId(
                 MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, it.mediaId.toLong())
-            it.buildUpon().setUri(uri).build()
+
+            val builder = it.buildUpon().setUri(uri)
+            if (metadata != null) builder.setMediaMetadata(metadata)
+            builder.build()
         }.toMutableList()
         return Futures.immediateFuture(updatedMediaItems)
     }
+
+    /**
+     * Retrieve the album artwork stored by the app for a given album ID.
+     * If no artwork is found then a default artwork image is returned instead.
+     *
+     * @param albumId The ID of the album that artwork should be retrieved for.
+     * @return A Bitmap representation of the album artwork.
+     */
+    /* private fun getArtworkByAlbumId(albumId: String?): Bitmap {
+        albumId?.let {
+            try {
+
+                val imageFile = File(directory, "$albumId.jpg")
+                if (imageFile.exists()) {
+                    return BitmapFactory.decodeStream(FileInputStream(imageFile))
+                }
+            } catch (_: Exception) { }
+        }
+        // If an error has occurred or the album ID is null, then return a default artwork image
+        return BitmapFactory.decodeResource(applicationContext.resources, R.drawable.no_album_artwork)
+    } */
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo) = mediaSession
 
@@ -102,95 +150,8 @@ class MediaPlaybackService : MediaSessionService(), MediaSession.Callback {
         stopSelf()
     }
 
-    /** Refresh the metadata displayed in the media player notification and handle user interactions. */
-    /* private fun refreshNotification() {
-        val isPlaying = mediaPlayer?.isPlaying ?: false
-        val playPauseIntent = if (isPlaying) {
-            Intent(applicationContext, MediaPlaybackService::class.java).setAction(ACTION_PAUSE)
-        } else Intent(applicationContext, MediaPlaybackService::class.java).setAction(ACTION_PLAY)
-        val nextIntent = Intent(applicationContext, MediaPlaybackService::class.java).setAction(ACTION_NEXT)
-        val prevIntent = Intent(applicationContext, MediaPlaybackService::class.java).setAction(ACTION_PREVIOUS)
-
-        val intent = packageManager
-            .getLaunchIntentForPackage(packageName)
-            ?.setPackage(null)
-            ?.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED)
-        val activityIntent = PendingIntent.getActivity(applicationContext, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-
-        val builder = NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID).apply {
-            val mediaMetadata = mediaSessionCompat.controller.metadata
-
-            // Previous button
-            addAction(
-                NotificationCompat.Action(R.drawable.ic_back, getString(R.string.play_prev),
-                    PendingIntent.getService(applicationContext, 0, prevIntent, PendingIntent.FLAG_IMMUTABLE)
-                )
-            )
-
-            // Play/pause button
-            val playOrPause = if (isPlaying) R.drawable.ic_pause
-            else R.drawable.ic_play
-            addAction(
-                NotificationCompat.Action(playOrPause, getString(R.string.play_pause),
-                    PendingIntent.getService(applicationContext, 0, playPauseIntent, PendingIntent.FLAG_IMMUTABLE)
-                )
-            )
-
-            // Next button
-            addAction(
-                NotificationCompat.Action(R.drawable.ic_next, getString(R.string.play_next),
-                    PendingIntent.getService(applicationContext, 0, nextIntent, PendingIntent.FLAG_IMMUTABLE)
-                )
-            )
-
-            setStyle(androidx.media.app.NotificationCompat.MediaStyle()
-                .setShowActionsInCompactView(0, 1, 2)
-                .setMediaSession(mediaSessionCompat.sessionToken)
-            )
-
-            val smallIcon = if (isPlaying) R.drawable.play
-            else R.drawable.pause
-            setSmallIcon(smallIcon)
-
-            setContentIntent(activityIntent)
-
-            // Add the metadata for the currently playing track
-            setContentTitle(mediaMetadata.getString(MediaMetadataCompat.METADATA_KEY_TITLE))
-            setContentText(mediaMetadata.getString(MediaMetadataCompat.METADATA_KEY_ARTIST))
-            setLargeIcon(mediaMetadata.getBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART))
-
-            // Make the transport controls visible on the lockscreen
-            setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-            priority = NotificationCompat.PRIORITY_DEFAULT
-        }
-        // Display the notification and place the service in the foreground
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU) {
-            // Pre-SDK 34
-            startForeground(1, builder.build())
-        } else {
-            // SDK 34 and up
-            startForeground(1, builder.build(), FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK)
-        }
-    } */
-
-    /**
-     * Retrieve the album artwork stored by the app for a given album ID.
-     * If no artwork is found then a default artwork image is returned instead.
-     *
-     * @param albumId The ID of the album that artwork should be retrieved for.
-     * @return A Bitmap representation of the album artwork.
-     */
-    /* private fun getArtworkByAlbumId(albumId: String?): Bitmap {
-        albumId?.let {
-            try {
-                val directory = ContextWrapper(applicationContext).getDir("albumArt", Context.MODE_PRIVATE)
-                val imageFile = File(directory, "$albumId.jpg")
-                if (imageFile.exists()) {
-                    return BitmapFactory.decodeStream(FileInputStream(imageFile))
-                }
-            } catch (_: Exception) { }
-        }
-        // If an error has occurred or the album ID is null, then return a default artwork image
-        return BitmapFactory.decodeResource(applicationContext.resources, R.drawable.no_album_artwork)
-    } */
+    override fun onUpdateNotification(session: MediaSession, startInForegroundRequired: Boolean) {
+        Log.d("DEBUG", "onUpdateNotification startInForeground: $startInForegroundRequired")
+        super.onUpdateNotification(session, startInForegroundRequired)
+    }
 }
