ff2d8ede4789a8582ef19fc7f360b2f0acbefd65
Adam <adamrh48@outlook.com>
2025-11-18 18:28:51 +0000

    Added backend changes to support editing the Album ID

--- app/src/main/java/com/codersguidebook/supernova/ui/album/EditAlbumFragment.kt
+++ app/src/main/java/com/codersguidebook/supernova/ui/album/EditAlbumFragment.kt
@@ -1,6 +1,5 @@
 package com.codersguidebook.supernova.ui.album
 
-import android.app.ProgressDialog.show
 import android.net.Uri
 import android.os.Bundle
 import android.text.SpannableStringBuilder
