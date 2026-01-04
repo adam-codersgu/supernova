ab9926494f5dfb12e20fb209b407059863610123
Adam <adamrh48@outlook.com>
2026-01-04 18:27:30 +0000

    Fixed bug with settings activity app bar obscuring content

--- app/src/main/java/com/codersguidebook/supernova/SettingsActivity.kt
+++ app/src/main/java/com/codersguidebook/supernova/SettingsActivity.kt
@@ -2,7 +2,11 @@ package com.codersguidebook.supernova
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import com.codersguidebook.supernova.databinding.SettingsActivityBinding
 import com.codersguidebook.supernova.ui.settings.SettingsFragment
 
@@ -19,9 +23,22 @@ class SettingsActivity : AppCompatActivity() {
             .replace(R.id.settings, SettingsFragment())
             .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                bottomMargin = insets.bottom
+                rightMargin = insets.right
+                topMargin = insets.top
+            }
+
+            WindowInsetsCompat.CONSUMED
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // TODO - THIS ISN'T NEEDED BUT SEE IF YOU CAN POP BACKSTACK IF APPLICABLE ON HOME PRESS?
         when (item.itemId) {
             android.R.id.home -> {
                 finish()
