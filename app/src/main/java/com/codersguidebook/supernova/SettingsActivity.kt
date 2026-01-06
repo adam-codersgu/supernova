ccb896fdb7a73106d97a5022cf78097b153b8222
Adam <adamrh48@outlook.com>
2026-01-06 18:06:27 +0000

    Enabled backwards navigation from preference fragment

--- app/src/main/java/com/codersguidebook/supernova/SettingsActivity.kt
+++ app/src/main/java/com/codersguidebook/supernova/SettingsActivity.kt
@@ -7,10 +7,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import com.codersguidebook.supernova.databinding.SettingsActivityBinding
 import com.codersguidebook.supernova.ui.settings.SettingsFragment
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
+
+    companion object {
+        const val PREFERENCE_FRAGMENT_TAG = "PreferenceFragment"
+    }
 
     private lateinit var binding: SettingsActivityBinding
 
@@ -37,12 +43,28 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 
+    override fun onPreferenceStartFragment(caller: PreferenceFragmentCompat, pref: Preference): Boolean {
+        val args = pref.extras
+        val fragment = supportFragmentManager.fragmentFactory.instantiate(
+            classLoader,
+            pref.fragment!!)
+        fragment.arguments = args
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.settings, fragment, PREFERENCE_FRAGMENT_TAG)
+            .addToBackStack(null)
+            .commit()
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // TODO - THIS ISN'T NEEDED BUT SEE IF YOU CAN POP BACKSTACK IF APPLICABLE ON HOME PRESS?
         when (item.itemId) {
             android.R.id.home -> {
-                finish()
-                return true
+                val preferenceFragment = supportFragmentManager.findFragmentByTag(PREFERENCE_FRAGMENT_TAG)
+                if (preferenceFragment != null && preferenceFragment.isVisible) {
+                    supportFragmentManager.popBackStack()
+                    return true
+                }
             }
         }
         return super.onOptionsItemSelected(item)
