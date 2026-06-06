094d2dd99a479a02b171ec7e3d9e7f4aef51fcb4
Adam <adamrh48@outlook.com>
2026-06-06 17:21:40 +0100

    Kotlin reflection

--- app/src/test/java/com/codersguidebook/supernova/testutils/ReflectionUtils.kt
+++ app/src/test/java/com/codersguidebook/supernova/testutils/ReflectionUtils.kt
@@ -1,13 +1,24 @@
 package com.codersguidebook.supernova.testutils
 
 import java.lang.reflect.Field
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.declaredFunctions
+import kotlin.reflect.jvm.isAccessible
 
 object ReflectionUtils {
 
+    // TODO MIGRATE TO KOTLIN REFLECTION? SEE IF THERE IS A DEPENDENCY TO DELETE
     fun setFieldVisible(targetObject: Any, fieldName: String): Field {
         val targetField = targetObject.javaClass.getDeclaredField(fieldName)
         targetField.isAccessible = true
         return targetField
+    }
+
+    fun setMethodVisible(targetObject: Any, methodName: String): KFunction<*> {
+        val targetMethod = targetObject::class.declaredFunctions
+            .first { it.name == methodName }
+        targetMethod.isAccessible = true
+        return targetMethod
     }
 
     fun replaceFieldWithMock(targetObject: Any, fieldName: String, mockObject: Any) {
